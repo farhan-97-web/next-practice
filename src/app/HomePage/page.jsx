212329872fd7564fe26625587b84bc59f2240a78
farhan-97-web <farhanchy822@gmail.com>
2025-01-24 15:48:28 -0800

    site updated

--- src/app/HomePage/page.jsx
+++ src/app/HomePage/page.jsx
@@ -18,12 +18,12 @@ const HomePage = async () => {
             <div className='grid lg:grid-cols-5'>
                 {
                     postData?.slice(0,20)?.map(post=>(
-                        <div key={post.id} className="card bg-base-100 w-full lg:w-60 shadow-xl">
+                        <div key={post?.id} className="card bg-base-100 w-full lg:w-60 shadow-xl">
   <div className="card-body">
-    <h2 className="card-title">Title : {post.title}</h2>
+    <h2 className="card-title">Title : {post?.title}</h2>
     
     <div className="card-actions justify-end items-end bottom-0">
-      <button className="btn btn-primary"><Link href={`/HomePage/${post.id}`}>View Details</Link></button>
+      <button className="btn btn-primary"><Link href={`/HomePage/${post?.id}`}>View Details</Link></button>
     </div>
   </div>
 </div>
