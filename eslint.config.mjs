4f21cd25fa1c52b86b493d317104afa56b8888ab
farhan-97-web <farhanchy822@gmail.com>
2025-01-24 16:11:14 -0800

    site updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const compat = new FlatCompat();
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-// eslint-disable-next-line import/no-anonymous-default-export
 export default [
   ...compat.extends("next/core-web-vitals"),
-  {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"], // Specify files to lint
-    ignores: ["node_modules/**", ".next/**", "out/**"], // Ignore unnecessary files
-    rules: {
-      // Add any custom rules or override defaults here
-    },
-  },
 ];
